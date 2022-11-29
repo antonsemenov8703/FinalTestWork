e5ae27c0470d560bf0bb7aa3a46e450d4e1bdcc7
Anton <anton-semenov@mail.ru>
2022-11-29 16:39:24 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//Console.WriteLine("Введите массив строк:");
+string [] array = {"Russia", "Denmark", "USA"};
+string [] resultArray = new string[10];
+for (int i=0; i<array.Length;i++)
+{
+    if (array[i].Length<=3)
+    {
+        Console.WriteLine($"{array[i]}");
+        resultArray[] = array[i];
+    }
+}
